69ada578646c8d6e3d6f6198bfd384d360fa733d
Volgena <ozdanovich1.21@gmail.com>
2022-10-29 22:32:17 +0300

    TestNG

--- src/main/java/org/example/Triangle.java
+++ src/main/java/org/example/Triangle.java
@@ -76,27 +76,3 @@ public  class Triangle {
     }
 }
 
-
-      /*if (aNum != 0 && bNum != 0 && cNum != 0) {
-            if (aNum + bNum > cNum && aNum + cNum > bNum && bNum + cNum > aNum) {
-                isTriangle = true;
-                System.out.println("It is a triangle!");
-                if (aNum == bNum && bNum == cNum) {
-                    triangleType = "Equilateral";
-                    System.out.println("It is an equilateral triangle");
-                } else if (aNum == bNum || bNum == cNum || aNum == cNum) {
-                    triangleType = "Isosceles";
-                    System.out.println("It is an isosceles triangle");
-                } else {
-                    System.out.println("It is a scalene triangle");
-                }
-            }
-        }
-
-        if (!isTriangle) {
-            System.out.println("It is not a triangle");
-        }
-
-    }*/
-
-
